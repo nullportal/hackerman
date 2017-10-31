d75fe2e6ce6290e7870666f543a4d87fc12656f9
JM <jmjanzen@gmail.com>
2017-10-31 13:36:40 -0400

    Revert - did not check this in correctly
    This reverts commit 32829d22b11182776a82de2f3792f2fc7083db99.

--- req/github.py
+++ req/github.py
@@ -6,16 +6,13 @@ class Github():
 
     base_api_url = "https://api.github.com/"
 
-    def __init__(self, client_credentials):
-        # Init with our credentials, or just use anauthenticated
-        try:
-            self.client_id, self.client_secret = client_credentials.split(':')
-        except:
-            self.client_id, self.client_secret = None, None
+    def __init__(self):
+        # TODO Set User-Agent, client_secret:key
+        pass
 
     def search(self, query_str, query_lang=None):
 
-        api_query = self._authenticate_req(self.base_api_url + "search/repositories?q=" + query_str)
+        api_query = self.base_api_url + "search/repositories?q=" + query_str
 
         # Add optional language filter
         if query_lang:
@@ -29,7 +26,7 @@ class Github():
     def get_contents(self, query):
 
         # Remove instructional tokens
-        query = self._authenticate_req(re.sub(r"\{.*\}", "", query))
+        query = re.sub(r"\{.*\}", "", query)
         print("Query:", query)
 
         resp = requests.get(query)
@@ -37,28 +34,8 @@ class Github():
 
     def get_raw(self, query):
 
-        query = self._authenticate_req(query)
         print("Query:", query)
 
         resp = requests.get(query)
         return resp.text
 
-    def get_limit(self):
-        query = self._authenticate_req(self.base_api_url + "rate_limit")
-        print("Query:", query)
-
-        resp = requests.get(query)
-        return json.loads(resp.text)
-
-    def _authenticate_req(self, q):
-        """ Add client secret/id to given query """
-        authenticated_query_url = q
-
-        # Append properly depending on what's already there
-        if self.client_id is not None and self.client_secret is not None:
-            if '?' in q or '&' in q:
-                authenticated_query_url = f"{q}&client_id={self.client_id}&client_secret={self.client_secret}"
-            else:
-                authenticated_query_url = f"{q}?client_id={self.client_id}&client_secret={self.client_secret}"
-
-        return authenticated_query_url
